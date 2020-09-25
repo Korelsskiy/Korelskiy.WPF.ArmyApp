b4635b6d863313b3573909488161e7e7be73eb73
Korelsskiy <68390165+Korelsskiy@users.noreply.github.com>
2020-09-25 11:30:49 +0300

    Создается разметка

---
+++ Infrastructure/Commands/Base/Command.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Korelskiy.WPF.ArmyApp.Infrastructure.Commands.Base
+{
+    internal abstract class Command : ICommand
+    {
+        public event EventHandler CanExecuteChanged // изменить возможность выполенения
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public abstract bool CanExecute(object parameter); // Проверяет, можно ли выполнить комманду
+
+        public abstract void Execute(object parameter); // то что должно быть выполнено командой, ее основная логика
+        
+    }
+}
---
+++ ViewModels/Base/ViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Korelskiy.WPF.ArmyApp.ViewModels.Base
+{
+    internal abstract class ViewModel : INotifyPropertyChanged // Этот интерфейс отслеживает какоето свойство и меняет его значение в визуальном интерфейсе 
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+    }
+}
